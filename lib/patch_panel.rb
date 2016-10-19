67c59da4516aa7886a11b8af926062b9f1b113b6
Ryosuke NARIMOTO <r-narimoto@ist.osaka-u.ac.jp>
2016-10-19 13:43:13 +0900

    modified patch_panel.rb

--- lib/patch_panel.rb
+++ lib/patch_panel.rb
@@ -19,61 +19,98 @@ class PatchPanel < Trema::Controller
 
   def create_patch(dpid, port_a, port_b)
     add_flow_entries dpid, port_a, port_b
-    @patch[dpid] << [port_a, port_b].sort
   end
 
   def delete_patch(dpid, port_a, port_b)
     delete_flow_entries dpid, port_a, port_b
-    @patch[dpid].delete([port_a, port_b].sort)
   end
 
   def create_mirror_patch(dpid, port, mirror)
     add_flow_mirror_entries dpid, port, mirror
-    @m_patch[dpid] << [port, mirror]
   end
 
   def delete_mirror_patch(dpid, port, mirror)
     delete_flow_mirror_entries dpid, port, mirror
-    @m_patch[dpid].delete([port, mirror])
   end
 
   def print_patch_mirror(dpid)
-    p "Patch list:"
-    p @patch[dpid]
-    p "Mirror list:"
-    p @m_patch[dpid]
+    p "Patch list: (port <=> port)"
+    @patch[dpid].each do |port_a, port_b|
+      print(port_a, " <=> ", port_b, "\n")
+    end
+    p "Mirror list: (port => mirror)"
+    @m_patch[dpid].each do |port, mirror|
+      print(port, " => ", mirror, "\n")
+    end
   end
 
   private
 
   def add_flow_entries(dpid, port_a, port_b)
+    @patch[dpid].each do |ports|
+      return if ports[0] == ([port_a, port_b].sort)[0]
+    end
     send_flow_mod_add(dpid,
                       match: Match.new(in_port: port_a),
                       actions: SendOutPort.new(port_b))
     send_flow_mod_add(dpid,
                       match: Match.new(in_port: port_b),
                       actions: SendOutPort.new(port_a))
+    @patch[dpid] << [port_a, port_b].sort
   end
 
   def delete_flow_entries(dpid, port_a, port_b)
+    is_no_entry = true
+    @patch[dpid].each do |ports|
+      is_no_entry = false if ports[0] == ([port_a, port_b].sort)[0]
+    end
+    return if is_no_entry
     send_flow_mod_delete(dpid,
                          match: Match.new(in_port: port_a),
                          actions: SendOutPort.new(port_b))
     send_flow_mod_delete(dpid,
                          match: Match.new(in_port: port_b),
                          actions: SendOutPort.new(port_a))
+    @patch[dpid].delete([port_a, port_b].sort)
   end
 
   def add_flow_mirror_entries(dpid, port, mirror)
+    @m_patch[dpid].each do |ports|
+      return if ports == [port, mirror]
+    end
+    port_src = nil
+    @patch[dpid].each do |port_a, port_b|
+      port_src = port_a if port_b == port
+      port_src = port_b if port_a == port
+    end
+    return if port_src == nil
     send_flow_mod_add(dpid,
-                      match: Match.new(out_port: port),
+                      match: Match.new(in_port: port_src),
                       actions: SendOutPort.new(mirror))
+    send_flow_mod_add(dpid,
+                      match: Match.new(in_port: port),
+                      actions: SendOutPort.new(mirror))
+    @m_patch[dpid] << [port, mirror]
   end
 
   def delete_flow_mirror_entries(dpid, port, mirror)
+    is_no_entry = true
+    @m_patch[dpid].each do |ports|
+      is_no_entry = false if ports == [port, mirror]
+    end
+    return if is_no_entry
+    port_src = nil
+    @patch[dpid].each do |port_a, port_b|
+      port_src = port_a if port_b == port
+      port_src = port_b if port_a == port
+    end
     send_flow_mod_delete(dpid,
-                         match: Match.new(out_port: port),
+                         match: Match.new(in_port: port_src),
                          actions: SendOutPort.new(mirror))
+    send_flow_mod_delete(dpid,
+                         match: Match.new(in_port: port),
+                         actions: SendOutPort.new(mirror))
+    @m_patch[dpid].delete([port, mirror])
   end
 
 
